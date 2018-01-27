f4478d37018d3349b515cd577138ce83fc802c77
KasaidisNunil <Kasidis_Nunil@live.com>
2018-01-27 15:02:44 +0700

    add and modify bank and Customer

---
+++ src/main/java/bank.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class bank {
+    private String bankName;
+
+    private Map<Integer, Customer> customers;
+
+    public Bank(String name){
+        this.bankName = name;
+        this.customers = new HashMap<Integer, Customer>();
+    }
+    public void addCustomer(Customer c){
+        customers.put(c.getId(),c);
+    }
+    public Customer findCustomerById(int custId){
+        return customers.get(custId);
+    }
+    public final boolean validateCustomer(int custId, int pin){
+        Customer customer = findCustomerById(custId);
+        if (customer != null && customer.match(pin)){
+            return true;
+        }
+        return false;
+    }
+}
